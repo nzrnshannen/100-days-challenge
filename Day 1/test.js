a0ea15ce07489265cb89f7c00cfe647c9c38bdcf
Shannen Nazareno <nzrnshannen@gmail.com>
2023-02-17 19:44:09 +0800

    added day 1 progress

---
+++ Day 1/test.js
@@ -0,0 +1,11 @@
+function bulb_on()
+{
+    document.getElementById("header").innerHTML="Lights on.";
+    document.getElementById("bulb").src="bulb_on.jpg";
+}
+
+function bulb_off()
+{
+    document.getElementById("header").innerHTML="Lights off.";
+    document.getElementById("bulb").src="bulb_off.jpg";
+}
