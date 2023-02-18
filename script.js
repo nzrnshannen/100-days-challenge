65fd821a43f573061a455948964c132bceb004ce
Shannen Nazareno <nzrnshannen@gmail.com>
2023-02-18 23:03:45 +0800

    added day 2

---
+++ script.js
@@ -0,0 +1,30 @@
+const body = document.body
+
+
+const div = document.querySelector('div')
+const Hi = document.querySelector('#hi')
+const Bye = document.querySelector('#bye')
+
+//Hi.remove()
+//Bye.remove()
+//
+
+//body.append("Hello, World!", " Bye!")
+//body.appendChild("Hello, World! ", "Bye!") <-- no result
+
+//const div = document.createElement('div')
+//const strong = document.createElement('strong')
+//strong.innerText=" Test 2"
+//div.innerHTML= "Hello, <strong>testing!</strong>"
+//div.append(strong)
+//body.append(div)
+
+//to see difference between textContent and innerText
+//const div = document.querySelector('div')
+//console.log(div.textContent)
+//console.log(div.innerText)
+
+
+
+
+
