f209dc4cc078745f360e5a67d88c138346db9007
Shannen Nazareno <nzrnshannen@gmail.com>
2023-02-25 22:48:01 +0800

    added getElementByClass property

--- Day 4/app.js
+++ Day 4/app.js
@@ -2,3 +2,8 @@
 
 const title = document.getElementById('main-heading');
 console.log(title);
+
+//getElementByClass();
+
+const listItem = document.getElementsByClassName('list-items');
+console.log(listItem);
